7b7f2d14fe02d9b625929a775bc165f89c6fae8f
Sergey Mankevich <101549041+srgmh@users.noreply.github.com>
2023-07-12 18:27:28 +0300

    Feature/crypto (#4)
    * Asset views
    
    * Asset views
    
    * Added unsubscribe endpoint to asset
    
    * Added SafeJWTAuthentication
    
    * Added signals
    
    * Added viewsets for suitcases and wallets
    
    * Connected debug toolbar
    
    * Divided into services Asset views
    
    * Fixed.
    
    * Added OrderViewset
    
    * Isort fixes
    
    * Added _create_response method to OrderService
    
    * Added atomic decorator to OrderService
    
    * Asset views
    
    * Divided into services Asset views
    
    * Fixed.
    
    * Added OrderViewset
    
    * Isort fixes
    
    * Added _create_response method to OrderService
    
    * Added atomic decorator to OrderService
    
    * Reworked OrderViewSet & OrderService

--- tradeapp_django/api_crypto/views.py
+++ tradeapp_django/api_crypto/views.py
@@ -1,13 +1,14 @@
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from api_crypto.serializers import (AssetSerializer, SuitcaseSerializer,
-                                    WalletSerializer)
+from api_crypto.serializers import (AssetSerializer, OrderSerializer,
+                                    SuitcaseSerializer, WalletSerializer)
 from api_crypto.services.assest_service import AssetService
+from api_crypto.services.order_service import OrderService
 from api_users.authentication import SafeJWTAuthentication
-from crypto.models import Suitcase, Wallet
+from crypto.models import Order, Suitcase, Wallet
 
 
 class AssetViewSet(viewsets.GenericViewSet,
@@ -19,7 +20,6 @@ class AssetViewSet(viewsets.GenericViewSet,
 
     @action(methods=['post'], detail=False, url_path='subscribe')
     def subscribe(self, request: Request) -> Response:
-
         asset_id = request.data.get('asset_id', None)
         result = AssetService.subscribe(asset_id, request.user)
 
@@ -27,14 +27,14 @@ class AssetViewSet(viewsets.GenericViewSet,
 
     @action(methods=['post'], detail=False, url_path='unsubscribe')
     def unsubscribe(self, request: Request) -> Response:
-
         asset_id = request.data.get('asset_id', None)
         result = AssetService.unsubscribe(asset_id, request.user)
 
         return Response(result)
 
 
-class WalletViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class WalletViewSet(mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
 
     serializer_class = WalletSerializer
     authentication_classes = (SafeJWTAuthentication, )
@@ -44,7 +44,8 @@ class WalletViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
         return Wallet.objects.filter(suitcase__user=user)
 
 
-class SuitcaseViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class SuitcaseViewSet(mixins.ListModelMixin,
+                      viewsets.GenericViewSet):
 
     serializer_class = SuitcaseSerializer
     authentication_classes = (SafeJWTAuthentication,)
@@ -52,3 +53,33 @@ class SuitcaseViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     def get_queryset(self):
         user = self.request.user
         return Suitcase.objects.filter(user=user)
+
+
+class OrderViewSet(mixins.ListModelMixin,
+                   viewsets.GenericViewSet):
+
+    serializer_class = OrderSerializer
+    authentication_classes = (SafeJWTAuthentication, )
+
+    def get_queryset(self):
+        """Get query set of orders created by current user."""
+        return Order.objects.filter(user=self.request.user)
+
+    @action(methods=['post'], detail=False, url_path='create_order')
+    def create_order(self, request: Request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = self.request.user
+        operation_type = serializer.validated_data['operation_type']
+        asset = serializer.validated_data['asset']
+        quantity = serializer.validated_data['quantity']
+        order = serializer.save(user=user)
+        result = OrderService.create_order(
+            user,
+            order,
+            operation_type,
+            asset,
+            quantity
+        )
+
+        return Response(result)
